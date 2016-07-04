233d0c6748830dc84da852def6c5805c7ae2b031
Jeremy Arimado <jarimado@gmail.com>
2016-07-04 21:41:44 +1000

    changed content

--- js/frankie.js
+++ js/frankie.js
@@ -2,10 +2,10 @@ var comments = {
     nice: [
           { string: "%c🙉 %c Want to know what my mum always said about giving up? 'Not Today!' ", styles: ["font-size: 20px", "font-size: 14px; font-family: Arial"]}
         , { string: "%c🙉 %c If the current bug has you in hell, keep coding.", styles: ["font-size: 20px", "font-size: 14px; font-family: Arial"]}
-        , { string: "%c🙉 %c I don't think you can build programs without suffering a little - you are both the problem and the solution.", styles: ["font-size: 20px", "font-size: 14px; font-family: Arial"]}
+        , { string: "%c🙉 %c Just do it! Yesterday you said tomorrow. JUST DO IT!", styles: ["font-size: 20px", "font-size: 14px; font-family: Arial"]}
         , { string: "%c🙉 %c You've gotten this far. What's another few bugs?", styles: ["font-size: 20px", "font-size: 14px; font-family: Arial"]}
-        , { string: "%c🙉 %c I love the way you hit the keys on the keyboard! Amazing! ", styles: ["font-size: 20px", "font-size: 14px; font-family: Arial"]}
-        , { string: "%c🙉 %c Is today a kick-ass day to code or what!  ", styles: ["font-size: 20px", "font-size: 14px; font-family: Arial"]}
+        , { string: "%c🙉 %c You're a good programmer. You're also amazingly gorgeous to look at. Take a break and look at a mirror for a bit.", styles: ["font-size: 20px", "font-size: 14px; font-family: Arial"]}
+        , { string: "%c🙉 %c Killin' it. Crushin' it. Smashin' it.", styles: ["font-size: 20px", "font-size: 14px; font-family: Arial"]}
     ],
     funny: [
           "%c🙉 %cPlumbers get payed a lot. Maybe you should give that a go?."
